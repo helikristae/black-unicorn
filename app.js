35ebb7d17264744fbffa05353cec2543c8518898
helikristae <helikrista@hotmail.com>
2024-11-17 23:31:15 +0200

    The task called 'The booking system project -> Phase 1' finished version

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
+// app.js
 import { Hono } from "https://deno.land/x/hono/mod.ts";
+import { registerUser } from "./routes/register.js"; // Import register logic
+const app = new Hono();
+
+// Serve the registration form
+app.get('/register', async (c) => {
+    return c.html(await Deno.readTextFile('./views/register.html'));
+});
+
+// Route for user registration (POST request)
+app.post('/register', registerUser);
+
+Deno.serve(app.fetch);
+
+// Run the app using the command:
+// deno run --allow-net --allow-env --allow-read --watch app.js
+
+// Run the app using the command:
+// deno run --allow-net --allow-env --allow-read --watch app.js
+
+// Run the app using the command:
+// deno run --allow-net --allow-env --allow-read --watch app.js
+
+
+/* import { Hono } from "https://deno.land/x/hono/mod.ts";
 import client from "./db/db.js";
 import * as bcrypt from "https://deno.land/x/bcrypt/mod.ts"; // For password hashing
 
@@ -46,4 +71,4 @@ Deno.serve(app.fetch);
 
 
 // Run the app using the command:
-// deno run --allow-net --allow-env --allow-read --watch app.js
+// deno run --allow-net --allow-env --allow-read --watch app.js */
